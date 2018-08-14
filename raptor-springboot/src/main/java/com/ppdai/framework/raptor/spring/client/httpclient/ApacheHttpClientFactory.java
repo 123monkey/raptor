6b51c78fe94a1c0ca5b4e4b275305d7d4874a2a3
张呈熹 <zhangchengxi@ppdai.com>
2018-08-14 16:42:48 +0800

    增加http选项,选择是否使用长连接

--- raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/client/httpclient/ApacheHttpClientFactory.java
+++ raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/client/httpclient/ApacheHttpClientFactory.java
@@ -3,14 +3,19 @@ package com.ppdai.framework.raptor.spring.client.httpclient;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.NoConnectionReuseStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author yinzuolong
  */
 public class ApacheHttpClientFactory {
+
+    @Value("${raptor.httpclient.keepAlive:true}")
+    private Boolean keepAlive;
 
     public CloseableHttpClient createHttpClient(HttpClientConnectionManager httpClientConnectionManager,
                                              RaptorHttpClientProperties httpClientProperties) {
@@ -23,12 +28,17 @@ public class ApacheHttpClientFactory {
 
         HttpRequestRetryHandler retryHandler = new DefaultHttpRequestRetryHandler(httpClientProperties.getRetryCount(),
                 httpClientProperties.isRequestSentRetryEnabled());
-
-        return HttpClientBuilder.create().disableContentCompression()
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create().disableContentCompression()
                 .disableCookieManagement()
                 .useSystemProperties()
                 .setRetryHandler(retryHandler)
                 .setConnectionManager(httpClientConnectionManager)
-                .setDefaultRequestConfig(defaultRequestConfig).build();
+                .setDefaultRequestConfig(defaultRequestConfig);
+
+        if(!keepAlive){
+            httpClientBuilder.setConnectionReuseStrategy(NoConnectionReuseStrategy.INSTANCE);
+        }
+
+        return httpClientBuilder.build();
     }
 }
