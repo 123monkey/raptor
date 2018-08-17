720b136c07b942a176446e243bc974194bcededb
张呈熹 <zhangchengxi@ppdai.com>
2018-08-17 16:27:13 +0800

    对于 bug: tDouble 这样子的字段在序列化为json的时候会出现两次: tDouble 和 tdouble ,之前的修复方式有问题,现在暂时不修复这个bug

--- raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/RaptorAutoConfiguration.java
+++ raptor-springboot/src/main/java/com/ppdai/framework/raptor/spring/RaptorAutoConfiguration.java
@@ -1,7 +1,5 @@
 package com.ppdai.framework.raptor.spring;
 
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ppdai.framework.raptor.spring.converter.RaptorJacksonMessageConverter;
 import com.ppdai.framework.raptor.spring.converter.RaptorMessageConverter;
 import org.springframework.beans.BeansException;
@@ -10,21 +8,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
  * @author yinzuolong
  */
 @Configuration
-public class RaptorAutoConfiguration implements ApplicationContextAware{
+public class RaptorAutoConfiguration implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
     @Bean
     @ConditionalOnMissingBean(RaptorMessageConverter.class)
     public RaptorMessageConverter createRaptorMessageConverter() {
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().applicationContext(applicationContext).build().configure(MapperFeature.AUTO_DETECT_GETTERS, false);
-        return new RaptorJacksonMessageConverter(objectMapper);
+        return new RaptorJacksonMessageConverter();
     }
 
     @Override
